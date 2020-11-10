24fb83c09b8e661e5091a6c2c30b366ab5186701
Jon Le Marquand <jon.lemarquand@gmail.com>
2020-11-10 21:53:44 +0000

    Basic homescreen

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,32 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap');
 
+body {
+  margin: 0;
+  padding: 0;
+  font-family: $primaryFont;
+}
+
+a {
+  width: 300px;
+  margin: 20px 0;
+  padding: 20px;
+  background: $purple;
+  color: $gold;
+  border: 5px solid $gold;
+  text-align: center;
+  font-weight: 900;
+  font-size: 2em;
+  text-transform: uppercase;
+  text-decoration: none;
+
+  &:hover {
+    background: $gold;
+    color: $purple;
+    cursor: pointer;
+  }
+}
 </style>
--- src/components/Homescreen/Homescreen.vue
+++ src/components/Homescreen/Homescreen.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>Hello World</div>
+    <div id="homescreen">
+        <img :src="require(`../../assets/chordtheory.png`)" alt="Chord Theory Logo"/>
+        <router-link to="/games">Begin</router-link>
+    </div>
 </template>
 
 <script>
@@ -9,5 +12,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+#homescreen {
+    background: $purple;
+    height: calc(100vh - 80px);
+    margin: 0 auto;
+    padding: 40px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+}
 </style>
