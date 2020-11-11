ce524350ffcae9faba20101ab2b5752fd2f71f74
Jon Le Marquand <jon.lemarquand@gmail.com>
2020-11-11 12:51:31 +0000

    Countdown to answer

--- src/components/Games/ThreeNoteChords/ThreeNoteChords.vue
+++ src/components/Games/ThreeNoteChords/ThreeNoteChords.vue
@@ -1,13 +1,34 @@
 <template>
         <nav-bar></nav-bar>
         <div id="threeNoteChords">
-            <h1>Three Note Chords</h1>
+            <transition mode="out-in">
             <div class="howToPlay" v-if="!playMode">
+                <h1>Three Note Chords</h1>
                 <h2>How to Play</h2>
                 <p>Once you click start, we'll generate a new chord. You then have 5 seconds to play the correct chord on your instrument.</p>
                 <p>It's self-scoring, so you only cheat yourself!</p>
                 <button @click="readyUp">Start</button>
             </div>
+            <div class="theGame" v-else>
+                <div class="chordSection">
+                    <p>Play</p>
+                    <div class="chord">{{ currentChord }}</div>
+                </div>
+                <transition mode="out-in">
+                    <div class="countdown" v-if="!answerActive">
+                        <p>Answer In:</p>
+                        <div class="chord">{{ countdown }}</div>
+                    </div>
+                    <div class="answer" v-else>
+                        <div class="chord">{{ currentAnswer }}</div>
+                    </div>
+                </transition>
+                <div class="scoreSection">
+                    <p>Current Score:</p>
+                    <div class="score">{{ rightScore }} - {{ wrongScore }}</div>
+                </div>
+            </div>
+            </transition>
         </div>
         <the-footer></the-footer>
 </template>
@@ -17,12 +38,34 @@ export default {
     data() {
         return {
             playMode: false,
+            currentChord: "Eb7",
+            currentAnswer: "Eb Db G",
+            answerActive: false,
+            countdown: 5,
+            rightScore: 0,
+            wrongScore: 0
         }
     },
     methods: {
         readyUp() {
             this.playMode = true;
+            setTimeout(() => {
+                this.countdown--;
+            }, 2000);
         }
+    },
+    watch: {
+        countdown: {
+            handler(value) {
+                    if (value > 0) {
+                        setTimeout(() => {
+                            this.countdown--;
+                        }, 1000);
+                    } else {
+                        this.answerActive = true;
+                    }
+            },
+        },
     }
 }
 </script>
@@ -35,16 +78,18 @@ export default {
     align-items: center;
     justify-content: flex-start;
     flex-grow: 1;
-    margin-top: 40px;
+    margin: 40px auto;
 }
 
-.howToPlay {
+.howToPlay, .countdown, .scoreSection, .answer, .chordSection {
     display: flex;
     flex-direction: column;
     align-items: center;
+}
 
+.howToPlay {
     button {
-        color: $white;
+        color: $gold;
         background: $purple;
         padding: 10px 20px;
         font-family: $primaryFont;
@@ -57,7 +102,38 @@ export default {
             color: $purple;
         }
     }
+}
 
+.theGame {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    flex-grow: 1;
+
+    p {
+        font-weight: 700;
+        font-size: 1.17em;
+    }
+
+.chord {
+    font-family: $primaryFont;
+    font-weight: 900;
+    font-size: 64px;
+}
+
+}
+
+.v-enter-from, .v-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+.v-enter-active, .v-leave-active {
+  transition: all 0.3s ease-out;
+}
+.v-enter-to, .v-leave-from {
+  opacity: 1;
+  transform: translateY(0);  
 }
 
 p {
